03639190b6402ee753005d01eeb161ccd16ae676
Denys Sherstiuk <denissherstjuk@gmail.com>
2025-04-02 14:04:55 +0300

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,9 +2,9 @@
 import { io } from "socket.io-client";
 
 // URL вашего сервера Socket.IO
-// const SOCKET_URL = "https://movie-quiz-game-be.onrender.com"; // use it on prod
+const SOCKET_URL = "https://movie-quiz-game-be.onrender.com"; // use it on prod
 // const SOCKET_URL = "http://192.168.88.73:3000";
-const SOCKET_URL = "https://movie-quiz-game-be.onrender.com/";
+// const SOCKET_URL = "http://192.168.3.6:3000";
 
 // Создаем экземпляр Socket.IO клиента
 export const socket = io(SOCKET_URL);
