36c83ea7f158a1555eec941a7b6533b1ba5d78f4
Denys Sherstiuk <denissherstjuk@gmail.com>
2025-04-08 17:54:05 +0300

    update 08 04 logo to players render score

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -4,11 +4,11 @@ import { useNavigate, useParams } from "react-router-dom";
 import Modal from "react-modal";
 import css from "./GamePage.module.css";
 
-const initialState = [
-  { socketId: null, points: 0, name: "Черепашки" },
-  { socketId: null, points: 0, name: "Черепушки" },
-  { socketId: null, points: 0, name: "Черемушки" },
-];
+// const initialState = [
+//   { socketId: null, points: 0, name: "Черепашки" },
+//   { socketId: null, points: 0, name: "Черепушки" },
+//   { socketId: null, points: 0, name: "Черемушки" },
+// ];
 
 export default function Themes() {
   const { session } = useParams();
@@ -20,7 +20,7 @@ export default function Themes() {
   const [selectedFrame, setSelectedFrame] = useState(0);
   const [playerName, setPlayerName] = useState(null);
   const [playerAnswer, setPlayerAnswer] = useState(null);
-  const [scoreTable, setScoreTable] = useState(initialState);
+  const [scoreTable, setScoreTable] = useState(null);
   const [gameEnd, setGameEnd] = useState(false);
   const [winnerName, setWinnerName] = useState(null);
   const [winnerPts, setWinnerPts] = useState(null);
@@ -66,67 +66,97 @@ export default function Themes() {
   );
 
   useEffect(() => {
-    setSocketId(socket.id);
-    socket.emit("game_join_room", session, socketId);
-    socket.emit("get_themes", session);
-  }, [session, socketId]);
+    setTimeout(() => {
+      setSocketId(socket.id);
+      socket.emit("game_join_room", session, socketId);
+      socket.emit("get_themes", session);
+      console.log(`transfer ${socketId}, ${session}`);
+    }, 300);
+  }, [socketId, session]);
 
   useEffect(() => {
     if (!session) return;
+    if (socket.connected) {
+      socket.on(
+        "all_themes",
+        (themes) => setThemes(themes),
+        console.log(themes)
+      );
 
-    socket.on("all_themes", (themes) => setThemes(themes));
+      socket.on("all_frames", (frame) => {
+        setFrames(frame);
+        setIsModalOpen(true);
+      });
 
-    socket.on("all_frames", (frame) => {
-      setFrames(frame);
-      setIsModalOpen(true);
-    });
+      socket.on("change_frame", () => setSelectedFrame((prev) => prev + 1));
+      // const setPoints = () => {
+      //   socket.on(
+      //     "all_points",
+      //     (score) => setScoreTable(score),
+      //     console.log("all_point")
+      //   );
+      // };
 
-    socket.on("change_frame", () => setSelectedFrame((prev) => prev + 1));
+      socket.on(
+        "all_points",
+        (score) => setScoreTable(score),
+        console.log("all_point")
+      );
+      // if (!scoreTable) setPoints();
 
-    socket.on("all_points", (score) => setScoreTable(score));
+      socket.on("player_answer", (playerName) => setPlayerName(playerName));
 
-    socket.on("player_answer", (playerName) => setPlayerName(playerName));
+      socket.on("answer_yes", () => {
+        setPlayerAnswer("верно!");
+        setTimeout(() => {
+          setPlayerName(null);
+          setPlayerAnswer(null);
+          setSelectedFrame(5);
+        }, 1000);
+        setTimeout(() => {
+          setIsModalOpen(false);
+          setSelectedFrame(0);
+          setFrames([]);
+        }, 5000);
+      });
 
-    socket.on("answer_yes", () => {
-      setPlayerAnswer("верно!");
-      setTimeout(() => {
-        setPlayerName(null);
-        setPlayerAnswer(null);
-        setSelectedFrame(5);
-      }, 1000);
-      setTimeout(() => {
-        setIsModalOpen(false);
-        setSelectedFrame(0);
-        setFrames([]);
-      }, 5000);
-    });
+      socket.on("answer_no", () => {
+        setPlayerAnswer("не верно!");
+        setTimeout(() => {
+          setPlayerName(null);
+          setPlayerAnswer(null);
+        }, 3000);
+      });
 
-    socket.on("answer_no", () => {
-      setPlayerAnswer("не верно!");
-      setTimeout(() => {
-        setPlayerName(null);
-        setPlayerAnswer(null);
-      }, 3000);
-    });
+      socket.on("get_points", (playerName) => {
+        socket.emit(
+          "player_points",
+          session,
+          playerName,
+          5 - selectedFrame,
+          socket.id
+        );
+        console.log("emitted:", session, playerName, 5 - selectedFrame);
+      });
 
-    socket.on("round_end", () => {
-      setTimeout(() => {
-        setIsModalOpen(false);
-        setSelectedFrame(0);
-        setFrames([]);
+      socket.on("round_end", () => {
+        setTimeout(() => {
+          setIsModalOpen(false);
+          setSelectedFrame(0);
+          setFrames([]);
+        }, 3000);
         socket.emit("get_themes", session);
-      }, 3000);
-    });
+      });
 
-    socket.on("end_game", (winner, pts) => {
-      setWinnerName(winner);
-      setWinnerPts(pts);
-      setGameEnd(true);
-      setTimeout(() => {
-        navigate("/");
-      }, 4000);
-    });
-
+      socket.on("end_game", (winner, pts) => {
+        setWinnerName(winner);
+        setWinnerPts(pts);
+        setGameEnd(true);
+        setTimeout(() => {
+          navigate("/");
+        }, 4000);
+      });
+    }
     return () => {
       socket.off("all_frames");
       socket.off("change_frame");
@@ -137,7 +167,7 @@ export default function Themes() {
       socket.off("get_points");
       socket.off("all_themes");
     };
-  }, [navigate, session, socketId]);
+  }, [navigate, session, socketId, scoreTable, themes, selectedFrame]);
 
   useEffect(() => {
     if (!socket.connected) {
@@ -150,6 +180,9 @@ export default function Themes() {
     };
   }, []);
 
+  // if (!scoreTable) {
+  //   return <div>Loading...</div>;
+  // }
   return (
     <div className={css.wrap}>
       {frames.length > 0 && (
@@ -184,12 +217,18 @@ export default function Themes() {
       <h1 className={css.title}>Страница игры</h1>
       <h2 className={css.menuTitle}>Таблица результатов</h2>
       <div className={css.scoreTable}>
-        {scoreTable.map((player) => (
-          <div key={player.name} className={css.playerWrap}>
-            <h3 className={css.playerName}>{player.name}</h3>
-            <h3 className={css.playerName}>{player.points}</h3>
-          </div>
-        ))}
+        {scoreTable ? (
+          scoreTable.map((player) => (
+            <div key={player.name} className={css.playerWrap}>
+              <h3 className={css.playerName}>
+                {player.name} {player.logo}
+              </h3>
+              <h3 className={css.playerName}>{player.points}</h3>
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
 
       {Object.keys(themes).length > 0 && (
@@ -203,7 +242,7 @@ export default function Themes() {
                   {movies.movies.map((movie, index) => (
                     <div key={index} className={css.btnWrap}>
                       <button disabled={movie.guessed} className={css.btn}>
-                        {movie.index}
+                        {movie.guessed ? movie.whoGuessed : movie.index}
                       </button>
                     </div>
                   ))}
