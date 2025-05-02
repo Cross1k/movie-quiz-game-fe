5a550ffc400da87185a07ac7dd4a062c5dc7275f
Denys Sherstiuk <denissherstjuk@gmail.com>
2025-05-02 17:59:50 +0300

    fe

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 // URL вашего сервера Socket.IO
 
-const SOCKET_URL = "https://movie-quiz-game-be.onrender.com"; // use it on prod
+const SOCKET_URL = "https://movie-quiz-game-fe.onrender.com"; // use it on prod
 // const SOCKET_URL = "http://192.168.88.73:3000";
 // const SOCKET_URL = "http://192.168.3.6:3000";
 // const SOCKET_URL = "https://movie-quiz-be.glitch.me/";
